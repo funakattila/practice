b59f86de03d628e2546aa9f53a80ed5e73b3f7be
funak <funak.attila@gmail.com>
2022-07-05 22:05:52 +0200

    add test data to input fields tests

---
+++ src/main/java/pages/TestData.java
@@ -0,0 +1,12 @@
+package pages;
+
+public class TestData {
+
+    public final static String firstA = "5223";
+    public final static  String firstB = "6318";
+    public final static  String firstExpectedValue = "11541";
+    public final static  String secondA = "Kispál";
+    public final static  String secondB = "Borz";
+    public final static  String secondExpectedValue = "NaN";
+
+}
