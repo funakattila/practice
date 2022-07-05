a356d7546706aba60bca80740c0adb19cbfa96df
funak <funak.attila@gmail.com>
2022-07-05 22:05:38 +0200

    create input fields tests

--- src/main/java/pages/InputFieldPage.java
+++ src/main/java/pages/InputFieldPage.java
@@ -1,4 +1,48 @@
 package pages;
 
-public class InputFieldPage {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import io.qameta.allure.Step;
+
+public class InputFieldPage extends Page{
+
+    private final String url = "http://demo.seleniumeasy.com/basic-first-form-demo.html";
+    private final By closeButton = By.id("at-cv-lightbox-close");
+
+    private final By sum1Field = By.id("sum1");
+
+    private final By sum2Field = By.id("sum2");
+
+    private final By getTotalButton = By.xpath("//*[@id=\"gettotal\"]//button");
+
+    private final By value = By.id("displayvalue");
+
+    public InputFieldPage(WebDriver driver, WebDriverWait wait) {
+        this.driver = driver;
+        this.wait = wait;
+    }
+
+    @Step("Navigate to the page")
+    public void navigate() {
+        driver.navigate().to(url);
+    }
+
+    @Step("Close the popup window")
+    public void closePopUp() {
+        driver.findElement(closeButton).click();
+    }
+
+    @Step("Try to add the two values")
+    public void addNumbers(String a, String b) {
+        driver.findElement(sum1Field).sendKeys(a);
+        driver.findElement(sum2Field).sendKeys(b);
+        driver.findElement(getTotalButton).click();
+    }
+
+    @Step("Get the value")
+    public String value() {
+        return driver.findElement(value).getText();
+    }
+
 }
--- src/test/java/tests/InputFieldTest.java
+++ src/test/java/tests/InputFieldTest.java
@@ -1,12 +1,47 @@
 package tests;
 
-public class InputFieldTest {
+import io.qameta.allure.Description;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import pages.InputFieldPage;
+import pages.TestData;
 
+@Epic("Final exam practice")
+@Feature("Test Input fields")
+public class InputFieldTest extends BaseTest {
+
+    @Test
+    @Description("Összeadás valid adatokkal")
+    @Order(1)
     public void TestSumTwoNumbers() {
+        InputFieldPage inputFieldPage = new InputFieldPage(driver, wait);
 
+        inputFieldPage.navigate();
+        inputFieldPage.closePopUp();
+        inputFieldPage.addNumbers(TestData.firstA, TestData.firstB);
+
+        String expected = TestData.firstExpectedValue;
+        String actual = inputFieldPage.value();
+
+        Assertions.assertEquals(expected, actual);
     }
 
+    @Test
+    @Description("Összeadás invalid adatokkal")
+    @Order(2)
     public void TestSumTwoInvalidInputs() {
+        InputFieldPage inputFieldPage = new InputFieldPage(driver, wait);
 
+        inputFieldPage.navigate();
+        inputFieldPage.closePopUp();
+        inputFieldPage.addNumbers(TestData.secondA, TestData.secondB);
+
+        String expected = TestData.secondExpectedValue;
+        String actual = inputFieldPage.value();
+
+        Assertions.assertEquals(expected, actual);
     }
 }
